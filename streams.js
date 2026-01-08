146fe136884a03bb204541148291ab313213c3bd
kreshnaakarmacharya <itsmekrishnaa10@gmail.com>
2026-01-08 15:19:16 +0545

    learn file read and write streams

---
+++ streams.js
@@ -0,0 +1,13 @@
+const fs=require('fs');
+
+const readStream = fs.createReadStream('./docs/blog.txt', {encoding:'utf8'});
+
+const writeStream=fs.createWriteStream('./docs/blog4.txt');
+readStream.on('data', (chunk) => {
+    console.log('----- New Chunk -----');
+    console.log(chunk.toString());
+   writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
+
+})
+
