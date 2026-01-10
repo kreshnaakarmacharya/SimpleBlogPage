e4cef2265ee25f87fff0f0001ddbb5ec4ddf8565
kreshnaakarmacharya <itsmekrishnaa10@gmail.com>
2026-01-10 12:58:29 +0545

    redirecting page

--- server.js
+++ server.js
@@ -14,12 +14,20 @@ const fs=require('fs');
     switch(req.url){
         case '/':
             path +='/index.html';
+            res.statusCode=200;
             break;
         case'/about':
             path +='/about.html';
+            res.statusCode=200;
+            break;
+        case'/about-me':
+            res.statusCode=301;
+            res.setHeader('Location','/about');
+            res.end();
             break;
         default:
             path +='/404.html';
+            res.statusCode=400;
             break;
     }
 
