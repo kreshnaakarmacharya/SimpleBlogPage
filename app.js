af70e12cf12e6fec309a61bc6b5557295b12d54e
kreshnaakarmacharya <itsmekrishnaa10@gmail.com>
2026-01-29 19:18:32 +0545

    add ejs

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express= require('express');
 //express app
 const app=express();
 
-//listen for request
+//register view engine
+app.set('view engine','ejs');
 
+//listen for request
 app.listen(3000);
 
 app.get('/',(req,res)=>{
