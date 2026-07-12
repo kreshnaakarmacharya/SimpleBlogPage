c4155b9c486c1da5672eb85fe9ebf72acd2aba17
kreshnaakarmacharya <156319029+kreshnaakarmacharya@users.noreply.github.com>
2026-07-12 11:24:33 +0545

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app=express();
 const PORT=3000;
 
 //connect to mongo db
-const dbURI = 'mongodb+srv://krishnaakarmacharya82_db_user:%40Krishnaa%23123@blogcluster.yzpnaw7.mongodb.net/nodetuts?appName=blogcluster';
+const dbURI = 'mongodb+srv://krishnaakarmacharya82_db_user:qNIoIK14D0tmuuqZ@blogcluster.yzpnaw7.mongodb.net/nodetuts?appName=blogcluster';
 
 async function connectDB() {
   try {
